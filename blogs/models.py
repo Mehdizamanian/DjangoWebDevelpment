f367f5a028b10d103a81f374225645c5b2af3846
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-02 12:07:49 +0330

    Post Class created in blogs app

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+  title=models.CharField(max_length=254)
+  description=models.TextField()
+  active=models.BooleanField(default=False)
+  author=models.CharField(max_length=70)
+  updated_time=models.DateField(auto_now=True)
+  created_time=models.DateField(auto_now_add=True)
+  
+
+
