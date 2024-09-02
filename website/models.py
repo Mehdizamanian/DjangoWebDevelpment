2ce900c844ce8a7299ef27f2e07727772f7c0a3c
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-02 11:47:40 +0330

    contact class created in website app but not makemigrations and migrate

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+  name=models.CharField(max_length=60)
+  email=models.EmailField(max_length=254)
+  subject=models.CharField(max_length=254)
+  message=models.TextField()
