b0c97712db8ff78c9a656f054fb927111df42277
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-02 11:49:26 +0330

    noting changed

--- website/models.py
+++ website/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-
-class Contact(models.Model):
-  name=models.CharField(max_length=60)
-  email=models.EmailField(max_length=254)
-  subject=models.CharField(max_length=254)
-  message=models.TextField()
+# Create your models here.
