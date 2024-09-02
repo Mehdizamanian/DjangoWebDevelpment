d8e775e2be440861f1f127872d2fb2e988812d9e
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-02 12:43:41 +0330

    customizing admin panel blogs for Post Class

--- blogs/admin.py
+++ blogs/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from .models import Post
 
 
+class PostAdmin(admin.ModelAdmin):
+  list_display=['id','title','author','active']
+  list_filter=['active']
+  search_fields=('title','description',)
+  date_hierarchy='created_time'
+  list_display_links=['id','title']
 
-
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 
 
