d6f4aaeed82dd66c0556ab904ec61c721919b9c8
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-08 13:39:47 +0330

    deleted auth filled in model & pycavth  file

--- blogs/models.py
+++ blogs/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
   title=models.CharField(max_length=254)
   description=models.TextField()
   active=models.BooleanField(default=False)
-  author=models.CharField(max_length=70)
   updated_time=models.DateField(auto_now=True)
   created_time=models.DateField(auto_now_add=True)
 
