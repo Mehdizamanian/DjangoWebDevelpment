20a621c667bd2d51381d8766b465b7814f24bca3
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-16 15:15:26 +0330

    i used persiantools madule to change dates postdetails.html filterlly

--- blogs/templatetags/blogs_tags.py
+++ blogs/templatetags/blogs_tags.py
@@ -1,6 +1,8 @@
 
 from django import template
 from blogs.models import Post
+from persiantools.jdatetime import JalaliDate
+
 
 register=template.Library()
 
@@ -19,3 +21,8 @@ def upper(value):
 def recent_post():
   posts=Post.objects.filter(active=True).order_by('-created_time')[:2]
   return {'posts':posts}
+
+
+@register.filter
+def jalilidate(value):
+  return JalaliDate(value).strftime("%Y/%m/%d")
