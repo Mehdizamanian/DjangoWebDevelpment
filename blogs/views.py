01878d3f97d2c820c2eef868c12283584ecfe88b
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-22 14:11:29 +0330

    commentclass view created in postdetail func and configured in temp

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,get_object_or_404
-from .models import Post
+from .models import Post , Comment
 from django.db.models import Q
+
 
 def blog(request,**kwargs):
   posts=Post.objects.filter(active=True)
@@ -22,5 +23,6 @@ def blog(request,**kwargs):
 
 def post_details(request , num):
   post=get_object_or_404(Post,id=num)
-  context={'post':post}
+  comments=Comment.objects.filter(active=True,post=post)
+  context={'post':post,'comments':comments}
   return render(request,'blogs/post-details.html',context)
