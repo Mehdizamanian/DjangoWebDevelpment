2307949cb5ab94011f411592a5edf21e955c27b3
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-23 17:02:49 +0330

    django simple captcha created successfully

--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('blogs/', include('blogs.urls')),
+    path('captcha/', include('captcha.urls')),
 ]
 
 if settings.DEBUG:
