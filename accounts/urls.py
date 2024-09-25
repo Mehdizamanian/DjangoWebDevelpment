a0e363dd741ab814b57ec79741ce8a0e0a202000
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-09-25 22:03:25 +0330

    signup created by Usercreationform django

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,12 @@
 accounts
 """
 from django.urls import path 
-from .views import loginview
+from .views import loginview,signupview
 
 
 app_name="accounts"
 
 urlpatterns = [
-    path('',loginview),
+    path('',loginview,name="login"),
+    path('signup/',signupview,name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def loginview(request):
-  return render(request,'accounts/registration.html')
+  return render(request,'accounts/login.html')
 
 # Create your views here.
+def signupview(request):
+  if request.method=='POST':
+    form=UserCreationForm(request.POST)
+    if form.is_valid():
+      messages.success(request,'your account created successfully to log in below')
+      form.save()
+      return redirect("accounts:login")
+  else:    
+    form=UserCreationForm
+  return render(request,'accounts/signup.html',{'form':form})
