f2a4c6b4f3cbad65544c16d217082bae589c4e03
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-10-11 23:22:25 +0330

    dashboard crude created with some mixins avaliable only for superusers

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,7 +5,7 @@ all urls begins with blogs/....
 
 
 from django.urls import path 
-from .views import blog , post_details
+from .views import blog , post_details,PostList,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 app_name="blogs"
 
 urlpatterns = [
@@ -13,5 +13,10 @@ urlpatterns = [
     path('posts/post-detail/<int:num>/',post_details , name="post-details"),
     path('category/<cat>/',blog ,name="category"),
     path('author/<auth>/',blog ,name="author"),
-
+    #admin dashboard
+    path('dashboard/',PostList.as_view(),name="dashboard"),
+    path('dashboard/detail/<int:pk>/',PostDetailView.as_view(),name="dashboard-detail"),
+    path('dashboard/create/',PostCreateView.as_view(),name="create"),
+    path('dashboard/edit/<int:pk>',PostUpdateView.as_view(),name="edite"),
+    path('dashboard/delete/<int:pk>/',PostDeleteView.as_view(),name="delete"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,15 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render ,get_object_or_404,redirect
 from .models import Post , Comment
 from .forms import CommentForm
 from django.db.models import Q
 from django.contrib import messages
+
+from django.views.generic import ListView , DetailView,UpdateView,CreateView,DeleteView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+
 
 
 def blog(request,**kwargs):
@@ -23,6 +30,8 @@ def blog(request,**kwargs):
   return render(request,'blogs/blog.html', context)
 
 
+
+
 def post_details(request , num):
   post=get_object_or_404(Post,id=num)
   comments=Comment.objects.filter(active=True,post=post)
@@ -38,3 +47,69 @@ def post_details(request , num):
 
   context={'post':post,'comments':comments,'form':form}
   return render(request,'blogs/post-details.html',context)
+
+
+
+
+
+
+
+
+
+
+# admin Dashbord 
+
+class PostList(UserPassesTestMixin,ListView):
+  model=Post
+  template_name='blogs/dashboard.html'
+  context_object_name='posts'
+
+  def test_func(self):
+      return self.request.user.is_superuser
+
+  # def get_queryset to get filter and search
+
+
+class PostDetailView(UserPassesTestMixin,DetailView):
+  model=Post
+  template_name='blogs/dashboard-detail.html'
+  context_object_name='post'
+
+  def test_func(self):
+      return self.request.user.is_superuser
+
+
+
+class PostCreateView(UserPassesTestMixin,SuccessMessageMixin,CreateView):
+  model=Post
+  template_name='blogs/dashboard-form.html'
+  fields='__all__'
+  success_url=reverse_lazy('blogs:dashboard')
+  success_message = "new Post created "
+
+  def test_func(self):
+      return self.request.user.is_superuser
+  
+
+
+class PostUpdateView(UserPassesTestMixin,SuccessMessageMixin,UpdateView):
+  model=Post
+  template_name='blogs/dashboard-form.html'
+  fields='__all__'
+  success_url=reverse_lazy('blogs:dashboard')
+  success_message = "one post updated successfully"
+
+  def test_func(self):
+      return self.request.user.is_superuser
+
+
+
+class PostDeleteView(UserPassesTestMixin,SuccessMessageMixin,DeleteView):
+  model=Post
+  template_name='blogs/dashboard-delete.html'
+  fields='__all__'
+  success_url=reverse_lazy('blogs:dashboard')
+  success_message = "one post Deleted successfully"
+
+  def test_func(self):
+      return self.request.user.is_superuser
